4c3f55046f1f40eb4eb624c261e612267b229e34
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-05-30 00:11:10 +0800

    update title; add mobile bg-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,21 @@ export default async function Page() {
     return (
         <main className="text-black w-screen h-screen flex flex-col">
             <div
-                className="w-full px-6 flex flex-col items-center z-10"
+                className="w-full px-6 flex-col items-center z-10 hidden md:flex"
                 style={{
                     backgroundImage: `url(/images/pattern-bg-desktop.png)`,
+                    backgroundRepeat: "round",
+                }}
+            >
+                <h1 className="text-white md:text-3xl text-2xl w-full text-center font-semibold md:py-8 my-6">
+                    IP Address Tracker
+                </h1>
+                <InputIP />
+            </div>
+            <div
+                className="w-full px-6 flex flex-col items-center z-10 md:hidden"
+                style={{
+                    backgroundImage: `url(/images/pattern-bg-mobile.png)`,
                     backgroundRepeat: "round",
                 }}
             >
